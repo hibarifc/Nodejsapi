24fdd53f5c700d7ac06a67ea6e82eb7cd72e93b7
hibarifc <bankho181@gmail.com>
2017-08-26 11:04:24 +0700

    Signed-off-by: hibarifc <bankho181@gmail.com>

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var mysql = require('mysql');
 
 var users = require('./users');
 /* ใช้ port 7777 หรือจะส่งเข้ามาตอนรัน app ก็ได้ */
-var port = process.env.PORT || 7777;
+// var port = process.env.PORT || 7777;
 //parse
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -20,14 +20,15 @@ var con = mysql.createConnection({
   password: process.env.DB_PASSWORD
 });
 
-con.connect(function(err){
-	if(err) throw err;
-	console.log("Connected!")
-})
+// con.connect(function(err){
+// 	if(err) throw err;
+// 		console.log("Connected!")
+// })
 
 /* Routing */
 app.get('/', function (req, res) {
     res.send('<h1>Hello Node.js</h1>');
+    console.log('Starting node.js on port ');
 });
 app.get('/index', function (req, res) {
     res.send('<h1>This is index page</h1>');
@@ -45,6 +46,6 @@ app.get('/user/:id',function(req,res){
 
  
 /* สั่งให้ server ทำการรัน Web Server ด้วย port ที่เรากำหนด */
-app.listen(port, function() {
-    console.log('Starting node.js on port ' + port);
-});
+// app.listen(port, function() {
+//     console.log('Starting node.js on port ' + port);
+// });
