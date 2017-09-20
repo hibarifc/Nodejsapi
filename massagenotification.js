3584d5c6945362ce43a92fe65b0f7c5ca42cb9de
hibarifc <bankho181@gmail.com>
2017-09-20 13:15:02 +0700

    Signed-off-by: hibarifc <bankho181@gmail.com>

--- massagenotification.js
+++ massagenotification.js
@@ -5,16 +5,6 @@ var gcm = require('node-gcm');
 
 exports.sandmassage = function(usersid){
 
-	var message = new gcm.Message();
-	message.addData('title', 'แจ้งเตือน');
-	message.addData('message', 'ได้รับรายการเช่า');
-	message.addData('content-available', true);
-	message.addData('data', { "username": "Satit", "message": "Hello world" });
-	// message.addData('notId', 2);
-	// message.addData('image', 'http://res.cloudinary.com/demo/image/upload/w_133,h_133,c_thumb,g_face/bike.jpg');
-	message.addData('image', 'http://www.pro.moph.go.th/w54/images/ICT/loadlogomoph.png');	
-
-	var sender = new gcm.Sender('AAAAwnJv_gg:APA91bF2awph8l2fpkCWpLL-KAhtVFTVI77awA4COlrG9xRylk_7dvkG3c5rTtvNuWt2vKlsJvQvpAx6B2zmpelp0-5d3_O67NehuwYzAyIy8W8sdG1uwwCx8LBO4PlR_cffqVDe05y6');
 	var con = mysql.createConnection({
   		host: process.env.DB_HOST,
   		user: process.env.DB_USER,
@@ -24,16 +14,27 @@ exports.sandmassage = function(usersid){
 
 	var sql = "SELECT token FROM users_tokendevice WHERE users_id = ?";
 	con.query(sql,[usersid],function(err,result){
-  
+
 		if(result[0]!=null)
 		{
+			var message = new gcm.Message();
+			message.addData('title', 'แจ้งเตือน');
+			message.addData('message', 'ได้รับรายการเช่า');
+			message.addData('content-available', true);
+			message.addData('data', { "username": "Satit", "message": "Hello world" });
+			// message.addData('notId', 2);
+			// message.addData('image', 'http://res.cloudinary.com/demo/image/upload/w_133,h_133,c_thumb,g_face/bike.jpg');
+			message.addData('image', 'http://www.pro.moph.go.th/w54/images/ICT/loadlogomoph.png');	
+
+			var sender = new gcm.Sender('AAAAwnJv_gg:APA91bF2awph8l2fpkCWpLL-KAhtVFTVI77awA4COlrG9xRylk_7dvkG3c5rTtvNuWt2vKlsJvQvpAx6B2zmpelp0-5d3_O67NehuwYzAyIy8W8sdG1uwwCx8LBO4PlR_cffqVDe05y6');
 			console.log(result[0].token);
 
 			sender.send(message, { registrationTokens: result[0].token }, function (err, response) {
     		if(err) console.error(err);
-    		else 	console.log(response);
+    		else 	console.log(response); 
+    		console.log("sandmassage complete");
 			});
-			console.log("sandmassage complete");
+			
 		}
     });
 	
