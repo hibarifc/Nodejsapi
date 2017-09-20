ef348a4f050747b6faa70730816648edea10305a
hibarifc <bankho181@gmail.com>
2017-09-20 11:44:21 +0700

    Signed-off-by: hibarifc <bankho181@gmail.com>

--- massagenotification.js
+++ massagenotification.js
@@ -2,16 +2,17 @@ var gcm = require('node-gcm');
 var mysql = require('mysql');
 
 
-var message = new gcm.Message();
-message.addData('title', 'แจ้งเตือน');
-message.addData('message', 'ได้รับรายการเช่า');
-message.addData('content-available', true);
-message.addData('data', { "username": "Satit", "message": "Hello world" });
-// message.addData('notId', 2);
-// message.addData('image', 'http://res.cloudinary.com/demo/image/upload/w_133,h_133,c_thumb,g_face/bike.jpg');
-message.addData('image', 'http://www.pro.moph.go.th/w54/images/ICT/loadlogomoph.png');
 
 exports.sandmassage = function(usersid){
+
+	var message = new gcm.Message();
+	message.addData('title', 'แจ้งเตือน');
+	message.addData('message', 'ได้รับรายการเช่า');
+	message.addData('content-available', true);
+	message.addData('data', { "username": "Satit", "message": "Hello world" });
+	// message.addData('notId', 2);
+	// message.addData('image', 'http://res.cloudinary.com/demo/image/upload/w_133,h_133,c_thumb,g_face/bike.jpg');
+	message.addData('image', 'http://www.pro.moph.go.th/w54/images/ICT/loadlogomoph.png');	
 
 	let token;
 	var sender = new gcm.Sender('AAAAwnJv_gg:APA91bF2awph8l2fpkCWpLL-KAhtVFTVI77awA4COlrG9xRylk_7dvkG3c5rTtvNuWt2vKlsJvQvpAx6B2zmpelp0-5d3_O67NehuwYzAyIy8W8sdG1uwwCx8LBO4PlR_cffqVDe05y6');
@@ -21,8 +22,8 @@ exports.sandmassage = function(usersid){
   		password: process.env.DB_PASSWORD
 	});
 
-	sql="SELECT token FROM users_tokendevice where users_id = ?";
-	con.query(sql,[usersid],function(err,result){
+	sql="SELECT * FROM users_tokendevice WHERE users_id = ?";
+	con.query(sql,[usersid],function(err, result){
 		console.log(result);
 		if(result!=null)
 		{
