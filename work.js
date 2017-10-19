f4ffcb1eb74ec142912061db2e82339f425df806
hibarifc <bankho181@gmail.com>
2017-10-19 14:50:49 +0700

    Signed-off-by: hibarifc <bankho181@gmail.com>

--- work.js
+++ work.js
@@ -159,7 +159,10 @@ exports.getWorkreview = function(req,res){
         password: process.env.DB_PASSWORD,
         database : process.env.DB_NAME
     });
-    sql="SELECT * FROM works_review where users_id_ranter = '?' order by rating DESC";
+    sql=`SELECT works_review.id,works_review.users_id_ranter,works_review.works_id,works_review.rating,works_review.review,users_detail.firstname,users_detail.lastname FROM works_review
+        inner join works on works_review.works_id = works.id
+        inner join users_detail on works.users_id_service = users_detail.id
+        where works_review.users_id_ranter = '?' order by rating DESC`;
 
     con.query(sql,[users_id_ranter],function(err,result){
         if(err) throw err;
